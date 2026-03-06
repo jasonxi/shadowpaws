c819b800107147124165e25fe159d8ebe01e2d4c
Jason Xi <10809598+jasonxi@users.noreply.github.com>
2026-03-06 21:49:37 +1100

    Update storage URLs in config.js

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 {
     "homeRefreshIntervalMs": 1800000,
-    "baseStorageUrl": "https://storage.googleapis.com/shadow-paws.firebasestorage.app/",
-    "providersUrl": "https://storage.googleapis.com/shadow-paws.firebasestorage.app/providers.json",
-    "versionsUrl": "https://storage.googleapis.com/shadow-paws.firebasestorage.app/versions.json",
-    "catesUrl": "https://storage.googleapis.com/shadow-paws.firebasestorage.app/cates.json",
+    "baseStorageUrl": "https://storage.googleapis.com/shadow-paws.appspot.com/",
+    "providersUrl": "https://storage.googleapis.com/shadow-paws.appspot.com/providers.json",
+    "versionsUrl": "https://storage.googleapis.com/shadow-paws.appspot.com/versions.json",
+    "catesUrl": "https://storage.googleapis.com/shadow-paws.appspot.com/cates.json",
     "tmdbApiKey": "a955a07ac16dfa9a1977c6e03c34b875",
     "tmdbBaseUrl": "https://api.themoviedb.org/3/",
     "tmdbImageBaseUrl": "https://image.tmdb.org/t/p/w500",
